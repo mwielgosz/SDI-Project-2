8126984b88806bf3f5dff618609f1a78a4258f6b
Mike Wielgosz <dev.mwielgosz@gmail.coom>
2014-02-12 18:19:12 -0500

    Add boolean function allowing user to choose to take multiple sled rides - User can choose to ride between 1 and 5 times - User cannot enter less than 1 or more than 5

--- wielgosz_michael_project2.js
+++ wielgosz_michael_project2.js
@@ -8,8 +8,9 @@
 
 // Initialize global variables
 var userName = "John Doe",
-    mySpeed = .12,
-    userWillWin = true;
+    numberOfRides = 1,
+    maxSledRides = 5,
+    multipleRides = false;
     
 // Funtion to compare input for users name against fake name
 var greetFriend = function(friendName) {
@@ -20,6 +21,40 @@ var greetFriend = function(friendName) {
     }
 };
 
+// Function returns false when only one ride or true if multiple rides
+var confirmNumberOfRides = function(moreThanOneRide, maxRides) {
+    var numRides = 1;
+
+    console.log("Right now, we're planning on riding down once, but I'll ride up to five times.");
+    // Loop until user selects between 1 and 5 rides
+    while (moreThanOneRide) {
+        numRides = parseInt(prompt("How many times would you like to ride down the hill?"));
+
+        if (numRides > 1 && numRides <= maxRides) {
+            // User selected between 1 and 5 rides, break loop
+            console.log("Perfect! " + numRides + " sounds good to me!");
+            numberOfRides = numRides;
+            break;
+        } else if (numRides > maxRides) {
+            // User selected over 5 rides, repeat loop
+            console.log("Sorry, but " + numRides + " is too many for me. I'm getting tired, but I'll ride down five more times today.");
+        } else if (numRides === 1) {
+            // User selected 1 ride, which is not multiple rides, exits loop
+            console.log("One ride is not considered going down the hill more than once! Oh well, one ride is fine with me.")
+            moreThanOneRide = false;
+        }
+    }
+
+    return moreThanOneRide;
+};
+
 console.log("Hello there! My name is Mike Wielgosz and I see that you're sledding alone. Come join us!")
 userName = prompt("We're all going to go sledding together. What is your full name?", userName);
 greetFriend(userName);
+
+console.log("While we're walking to the hill, let's figure out how many times we should sled down.");
+multipleRides = confirm("Would you like to go down the hill more than once?");
+// Confirms user actually wants to ride down more than once.
+multipleRides = confirmNumberOfRides(multipleRides, maxSledRides);
+
+console.log("Well, we're at the top of the hill. This is going to be one heck of a sled ride!");
