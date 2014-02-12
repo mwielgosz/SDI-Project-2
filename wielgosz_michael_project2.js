77f75e05ff911872d931b6e616811d9980532590
Mike Wielgosz <dev.mwielgosz@gmail.coom>
2014-02-12 16:25:00 -0500

    Add prompt and function to handle user name

--- wielgosz_michael_project2.js
+++ wielgosz_michael_project2.js
@@ -1,6 +1,25 @@
-alert("JavaScript works!");
+//alert("JavaScript works!");
 
 // Michael Wielgosz
 // SDI 1402
 // Project 2
-// <NAME>
+// Sled Race
+
+
+// Initialize global variables
+var userName = "John Doe",
+    mySpeed = .12,
+    userWillWin = true;
+    
+// Funtion to compare input for users name against fake name
+var greetFriend = function(friendName) {
+    if (friendName === "John Doe") {
+        console.log("Even though I really doubt your real name is " + userName + ", it really doesn't matter. Let's go check out this awesome hill we found.");
+    } else {
+        console.log("It's nice to meet you " + userName + ". We found this great hill so grab your sled and let's check it out!");
+    }
+};
+
+console.log("Hello there! My name is Mike Wielgosz and I see that you're sledding alone. Come join us!")
+userName = prompt("We're all going to go sledding together. What is your full name?", userName);
+greetFriend(userName);
